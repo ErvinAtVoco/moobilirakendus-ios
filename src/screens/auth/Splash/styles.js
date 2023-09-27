a8af024f94f4a454d76268694b16fb2262c35e4a
ErvinAtVoco <ervin.tombak@voco.ee>
2023-09-27 13:15:46 +0300

    create splash screen with button

---
+++ src/screens/auth/Splash/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 9,
+        marginTop: 120
+    },
+    image: {
+        width: '100%',
+        height: 200
+    },
+    titleContainer: {
+        marginVertical: 54
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    innerTitle: {
+        color: '#FCA34d',
+        textDecorationLine: 'underline'
+    },
+    footerText: {
+        color:'#4F63AC',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTom: 30
+    }
+})
