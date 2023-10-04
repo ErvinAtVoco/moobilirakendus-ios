d1704f128fe66c6aba0403378be79557b516546e
ErvinAtVoco <ervin.tombak@voco.ee>
2023-10-04 13:04:04 +0300

    Added Signup, Input component, Updated AuthHeader

--- src/components/AuthHeader/index.js
+++ src/components/AuthHeader/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Pressable, Image, Text, View, Pressable } from "react-native";
+import { Pressable, Image, Text, View } from "react-native";
 import { styles } from "./styles";
 
-const AuthHedear = ({title, onBackPress}) => {
+const AuthHeader = ({title, onBackPress}) => {
     return (
         <View style={styles.container}>
             <Pressable hitSlop={20} onPress={onBackPress}>
-                <Image styles={styles.Image}source={require('../../assets/auth_back.png')}/>
+                <Image style={styles.image} source={require('../../assets/auth_back.png')}/>
             </Pressable>
             <Text style={styles.title}>{title}</Text>
         </View>
     )
 };
+
+export default AuthHeader;
